73f37038cd94f799a8d07881232e5034a7bef3e7
Tobi John <tobioluwafemi58@gmail.com>
2023-06-04 21:57:26 +0100

    Adds matrix API file

---
+++ linearR/src/main.rs
@@ -0,0 +1,28 @@
+use easy_ml:matrices::Matrix;
+
+let x: Matrix<f32> = Matrix::Column(
+    vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0]
+)
+
+let y = x.map(|x| x.powi(2) + x.sin());
+println!("{:?}", &y);
+
+let mut X = x.clone();
+
+X.insert_column(0, 1.0);
+
+X.insert_column_with(2, x.column_iter(0).map(|x| x * x))
+println!("{:?}", &X);
+
+
+let w = (X.transpose() * &X).inverse().unwrap() * (X.transpose() * &y);
+
+let predictions = &X * &w;
+
+let errors = &y - &predictions;
+
+let mean_squared_error = (errors.transpose() * &errors).get
+
+fn main() {
+    println!("Hello, world!");
+}
