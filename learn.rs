ed87ad82e86ecc8a47f057f8b6565b21e0e5e203
Tobi John <tobioluwafemi58@gmail.com>
2023-10-16 09:21:01 +0100

    Edits learn

--- learn.rs
+++ learn.rs
@@ -51,3 +51,36 @@ pub(crate) async fn my_handler(event: LambdaEvent<Request>) -> Result<Response, 
     Ok(resp)
 }
 
+// It is possible to declare variable bindings first, and initialize them later.
+// However, this form is seldom used, as it may lead to the use of uninitialized variables.
+fn main() {
+    // Declare a variable binding
+    let a_binding;
+
+    {
+        let x = 2;
+        // Initialize the binding 
+        a_binding = x * x;
+    }
+
+    println!("a binding: {}", a_binding);
+
+    let another_binding;
+
+// Error! Use of uninitialized binding
+println!("another binding: {}", another_binding);
+// FIXME ^ Comment out this line
+
+another_binding = 1;
+println!("another binding: {}", another_binding);
+
+} 
+
+
+function getUserData(userId) {
+    return fetch(`https://jsonplaceholder.typicode.com/users/${userID}`)
+        .then(response => response.json())
+        .catch(error => console.error('Error:', error));
+}
+
+
