8d9f4c29c2829388f1f834f02aeee6da2633cd5d
John.Oluwafemi <John.Oluwafemi@sainsburysbank.co.uk>
2023-10-20 14:39:40 +0100

    adds traint do dynamo public struct

---
+++ model.rs
@@ -0,0 +1,27 @@
+//! Dynamo DB Event models
+//!
+//! Models for the DynamoDB event entrypoint.
+//!
+//! We cannot use the models provided by the AWS SDK for Rust, as they do not
+//! implement the `serde::Serialize` and `serde::Deserialize` traits.
+
+use crate::{
+    model::{Event, Product},
+    Error,
+};
+use serde::{Deserialize, Serialize}
+use std::collections::HashMap;
+
+
+#[derive(Deserialize, Serialize, Debug)]
+
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct DynamoDBEvent {
+    #[serde(rename = "awsRegion")]
+    pub aws_region: String,
+
+    
+
+
+}
