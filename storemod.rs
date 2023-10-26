458a1e48b300f2c629748f477dbd3aed110ad138
John.Oluwafemi <John.Oluwafemi@sainsburysbank.co.uk>
2023-10-26 09:13:27 +0100

    Adds comments on store traits used

--- storemod.rs
+++ storemod.rs
@@ -9,3 +9,14 @@ pub use memory::MemoryStore;
 
 pub trait Store: StoreGetAll + StoreGet + StorePut + StoreDelete {}
 
+/// Trait for retreiving all products
+///
+/// this trait is implemented by the different storage  backends. It provides
+/// the basic interface for retrieving all products.
+///
+/// A given store could return only a partial different storage backends. It provides
+/// the basic interface for retrieving all products.
+/// 
+/// A given store could return only a partial list of all the products. If
+/// this is the case, the `next` parameter should be used to retrieve the 
+/// next page of products.
