3844ad5ff2d8c3bf08b96cd0b11bce36843745b5
John.Oluwafemi <John.Oluwafemi@sainsburysbank.co.uk>
2023-11-03 16:17:55 +0000

    Adds pub async function

--- domain.rs
+++ domain.rs
@@ -7,3 +7,13 @@ use crate::{
     store::{StoreDelete, StoreGet, StoreGetAll, StorePut:}
 }
 
+pub async fn get_product(
+    store: &dyn StoreGetAll,
+    next: Option<&str>,
+) -> Result<ProductRange, Error< {
+    store.all(next).await
+}
+
+pub async fn get_product(store: &dyn StoreGet, id: &str) -> Result<Option<Product>, Error> {
+    store.get(id).await
+}
