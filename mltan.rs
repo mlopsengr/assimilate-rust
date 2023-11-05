8553f1e8ec57abaa7168f323a99ad23a568e9b1f
Tobi John <tobioluwafemi58@gmail.com>
2023-11-05 21:43:17 +0000

    defines dataset path

--- mltan.rs
+++ mltan.rs
@@ -1,5 +1,6 @@
 use tangram::prelude::*;
 
 fn main() {
-    //
+    // Create an in-memory dataset for binarry classification
+    let dataset = tangram::BinaryClassificationDataset::from_path("path_to_your_dataset").unwrap()
 }
