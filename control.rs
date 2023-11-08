6b2cce9061f2301c3dc067bd6df1eea26c22cefe
John.Oluwafemi <John.Oluwafemi@sainsburysbank.co.uk>
2023-11-08 09:10:03 +0000

    completes function

--- control.rs
+++ control.rs
@@ -1,4 +1,4 @@
-fn main()
+fn main() {
     let number = 7
     
     if number % 2 == 0 {
@@ -11,6 +11,10 @@ fn main()
     match number {
         0 => println("It is zero"),
         1 | 2 => println!("It is one or 2"),
+        _ => println!("It is something else"),
+    }
 
-    
+    for i in 1..=5 {
+    println!("{}", i);
+    }
 }
