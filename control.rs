8e549c455302a54865830108dccd54de11aadc5f
John.Oluwafemi <John.Oluwafemi@sainsburysbank.co.uk>
2023-11-08 09:07:48 +0000

    Adds it is one or 2

--- control.rs
+++ control.rs
@@ -1,2 +1,16 @@
 fn main()
-    let number = 7;
+    let number = 7
+    
+    if number % 2 == 0 {
+        println!("Number is even")
+    } else {
+        println!("Number is odd");
+    }
+
+    // Match expression
+    match number {
+        0 => println("It is zero"),
+        1 | 2 => println!("It is one or 2"),
+
+    
+}
