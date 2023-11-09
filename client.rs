d127d86d637e5f979349d343bf05e0496b187bd6
John.Oluwafemi <John.Oluwafemi@sainsburysbank.co.uk>
2023-11-09 08:22:14 +0000

    imports futures

--- client.rs
+++ client.rs
@@ -3,3 +3,7 @@ use crate::server::{Request, Response};
 use crate::sql::engine::Status;
 use crate::sql::execution::ResultSet;
 use crate::sql::schema::Table;
+
+use futures::future::FutureExt as _;
+use futures::sink::SinkExt as _;
+use futures::stream::TryStreamExt as _;
