363f6c0ec9c8b79460c19eb343397f1d5e4b1339
Tobi John <tobioluwafemi58@gmail.com>
2023-11-10 19:18:02 +0000

    completes struct

--- distros.rs
+++ distros.rs
@@ -4,5 +4,7 @@ use clap::Parser;
 #[derive(Parser)]
 #[clap(version = "1.0", author = "Noah Gift", about = "hello world")]
 struct Cli {
-
-}
+    #[clap(subcommand)]
+    command: Option<Commands>,
+}   #[clap(subcommand)]
+command: Option<Commands>,
