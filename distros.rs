1a3d9a1d6ab94a62a052c34caa808045c490afe7
Tobi John <tobioluwafemi58@gmail.com>
2023-11-10 19:16:51 +0000

    begins Cli struct

--- distros.rs
+++ distros.rs
@@ -2,4 +2,7 @@
 use clap::Parser;
 
 #[derive(Parser)]
-#[clap(version = "1.0", author = "Noah Gift", about = "hello 
+#[clap(version = "1.0", author = "Noah Gift", about = "hello world")]
+struct Cli {
+
+}
