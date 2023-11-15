9a1aa36dc763dd725c896c18973eee97f5393d9b
John.Oluwafemi <John.Oluwafemi@sainsburysbank.co.uk>
2023-11-15 10:57:02 +0000

    Adds required modules and submodules

---
+++ burntoch.rs
@@ -0,0 +1,11 @@
+#[cfg(feature = "tch-gpu")]
+mod tch_gpu {
+    use burn_autodiff::Autodiff;
+    use burn_tch::{Libtorch, LibTorchDevice};
+    use mnist::training;
+
+    pub fn run() {
+
+    }
+    
+}
