7271c258b8be8800ac2d3604ca6ad0232baa1a00
John.Oluwafemi <John.Oluwafemi@sainsburysbank.co.uk>
2023-11-25 20:39:18 +0000

    Adds rustc color and edition args

--- exercise.rs
+++ exercise.rs
@@ -6,3 +6,6 @@ use std:fs::{self, remove_file, File};
 use std:io::Read;
 use std::path::PathBuf;
 use std::process::{self, Command};
+
+const RUSTC_COLOR_ARGS: &[&str] = &["--color", "always"];
+const RUSTC_EDITION_ARGS: &[&str] = &["--edition", "2021"];
