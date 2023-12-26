0ce6f5da970e90433acc3f83340f2b4e71601c80
TobiJohn <tobioluwafemi58@gmail.com>
2023-12-26 21:34:45 +0000

    allows unused mutable objects

--- LINEAR/src/main.rs
+++ LINEAR/src/main.rs
@@ -1,21 +1,2 @@
 
-
-const LIFE: u32 = 8;
-const DEATH: i32 = -28;
-
-
-
-// add a main function
-fn main() {
-    println!("{}", DEATH);
-    println!("{}", LIFE);
-
-    return;
-}
-
-let mut s1 = String::from("abc");
-do_stuff(&mut s1);
-
-fn do_stuff(s: &mut String) {
-    s.insert_str(0, "Hi, ");
-}
+#![allow(unused_mut)]
