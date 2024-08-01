846bac2e5a095dd57a8dbd213d754dafb14e0361
Tobi John <66161461+mlopsengr@users.noreply.github.com>
2024-08-01 21:42:01 +0100

    Update client.rs

--- client.rs
+++ client.rs
@@ -4,7 +4,6 @@ use crate::sql::engine::Status;
 use crate::sql::execution::ResultSet;
 use crate::sql::schema::Table;
 
-
 use futures::future::FutureExt as _;
 use futures::sink::SinkExt as _;
 use futures::stream::TryStreamExt as _;
